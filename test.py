4c917f2ff423ed9a4d4e2eed9ec2eaf2bbae1c8b
JJTech0130 <james@erinpryorgill.com>
2020-04-12 17:11:12 -0400

    Test script

---
+++ test.py
@@ -0,0 +1,17 @@
+import llfp, time
+test = llfp.leap("192.168.0.5")
+test.login("jjtech","jjtech0130")
+i = 0
+while i <= 100:
+    test.goToLevel(690,i,"00:00:01")
+    time.sleep(0.1)
+    i = i + 1
+test.goToLevel(690,50)
+while True:
+    if i == 100:
+        i = 0
+        test.goToLevel(690,i,"00:00:01")
+    else:
+        i = 100
+        test.goToLevel(690,i,"00:00:01")
+    time.sleep(1)
