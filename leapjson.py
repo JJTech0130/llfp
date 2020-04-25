7012f4fdb1fe72e3a09acdceef7aca92072b706e
JJTech0130 <james@erinpryorgill.com>
2020-04-25 09:59:05 -0400

    Color Support
    Added color support

--- leapjson.py
+++ leapjson.py
@@ -4,3 +4,4 @@
 loginPacket = """{"CommuniqueType": "UpdateRequest","Header": {"Url": "/login"},"Body": {"Login": {"ContextType": "Application","LoginId": "%s","Password": "%s"}}}\r\n"""
 goToLevelPacket = """{"CommuniqueType":"CreateRequest","Header":{"Url":"/zone/%d/commandprocessor"},"Body":{"Command":{"CommandType":"GoToDimmedLevel","DimmedLevelParameters":{"Level":%d,"FadeTime":"%s","DelayTime":"%s"}}}}\r\n"""
 pingPacket = """{"CommuniqueType":"ReadRequest","Header":{"Url":"/server/status/ping"}}\r\n"""
+goToColorPacket = """{"CommuniqueType":"CreateRequest","Header":{"Url":"/zone/%d/commandprocessor"},"Body":{"Command":{"CommandType":"GoToSpectrumTuningLevel","SpectrumTuningLevelParameters":{"Level":%d,"Vibrancy":%d,"ColorTuningStatus":{"HSVTuningLevel":{"Hue":%d,"Saturation":%d}},"DelayTime":"%s","FadeTime":"%s"}}}}\r\n"""
--- test.py
+++ test.py
@@ -1,17 +1,10 @@
-import llfp, time
-test = llfp.leap("192.168.0.5")
-print(test.login("loginID","password"))
-#i = 0
-#while i <= 100:
-#    test.goToLevel(690,i,"00:00:01")
-#    time.sleep(0.1)
-#    i = i + 1
-#test.goToLevel(690,50)
-#while True:
-#    if i == 100:
-#        i = 0
-#        test.goToLevel(690,i,"00:00:01")
-#    else:
-#        i = 100
-#        test.goToLevel(690,i,"00:00:01")
-#    time.sleep(1)
+# NOTE: You will need to put llfp.py and leapjson.py in the
+#  same folder as basic.py in order for this to work!!!
+import llfp
+# Setup connection and login
+bridge1 = llfp.bridge("192.168.0.5")
+print(bridge1.login("jjtech", "jjtech0130")) #Replace with your username/password.
+# Control zone
+zone1 = llfp.zone(6164, bridge1) #Replace 690 with the number of the zone you want to control.
+#print(zone1.goToColorFull(100,50,237,100)) #Replace 0 with a number 0-100
+print(zone1.goToColor(100,309))
