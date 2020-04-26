73cc76dcf502dd2f589ce7cb700b0ebeb2d392e3
JJTech0130 <james@erinpryorgill.com>
2020-04-26 09:00:58 -0400

    Tryed to correctly package

--- leapjson/leapjson.py
+++
@@ -1,7 +0,0 @@
-# LEAP JSON File
-# Declares the different JSON structures used for messages.
-# Used by LLFP (Lutron LEAP For Python)
-loginPacket = """{"CommuniqueType": "UpdateRequest","Header": {"Url": "/login"},"Body": {"Login": {"ContextType": "Application","LoginId": "%s","Password": "%s"}}}\r\n"""
-goToLevelPacket = """{"CommuniqueType":"CreateRequest","Header":{"Url":"/zone/%d/commandprocessor"},"Body":{"Command":{"CommandType":"GoToDimmedLevel","DimmedLevelParameters":{"Level":%d,"FadeTime":"%s","DelayTime":"%s"}}}}\r\n"""
-pingPacket = """{"CommuniqueType":"ReadRequest","Header":{"Url":"/server/status/ping"}}\r\n"""
-goToColorPacket = """{"CommuniqueType":"CreateRequest","Header":{"Url":"/zone/%d/commandprocessor"},"Body":{"Command":{"CommandType":"GoToSpectrumTuningLevel","SpectrumTuningLevelParameters":{"Level":%d,"Vibrancy":%d,"ColorTuningStatus":{"HSVTuningLevel":{"Hue":%d,"Saturation":%d}},"DelayTime":"%s","FadeTime":"%s"}}}}\r\n"""
--- llfp.py
+++ llfp.py
@@ -9,7 +9,7 @@ https://github.com/JJTech0130/LLFP
 '''
 
 import json
-from leapjson import leapjson
+import leapjson
 import socket
 import ssl
 import sys
